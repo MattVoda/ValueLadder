b0aac5344ee87a640e6481e83265053319971711
MattVoda <mattvodakov@gmail.com>
2017-09-25 22:44:53 -0400

    Adding correctly, onUngrab of interactable item. TODO: reset slider

--- Assets/VRTK/Examples/ExampleResources/Scripts/ControlReactor.cs
+++ Assets/VRTK/Examples/ExampleResources/Scripts/ControlReactor.cs
@@ -5,9 +5,18 @@
 
     public class ControlReactor : MonoBehaviour
     {
-        public TextMesh go;
+        public Adder Adder;
+        public TextMesh dispalyTextMesh;
+
+        private float currentPreviewNumber;
 
         private VRTK_Control_UnityEvents controlEvents;
+        private VRTK_InteractableObject_UnityEvents interactableObjEvents;
+
+        private void Awake() {
+            Adder = transform.root.GetComponentInChildren<Adder>();
+            print("adder added");
+        }
 
         private void Start()
         {
@@ -16,13 +25,33 @@
             {
                 controlEvents = gameObject.AddComponent<VRTK_Control_UnityEvents>();
             }
+            controlEvents.OnValueChanged.AddListener(HandleLivePreviewChange);
 
-            controlEvents.OnValueChanged.AddListener(HandleChange);
+
+            interactableObjEvents = GetComponent<VRTK_InteractableObject_UnityEvents>();
+            if (interactableObjEvents == null) {
+                interactableObjEvents = gameObject.AddComponent<VRTK_InteractableObject_UnityEvents>();
+            }
+            
+            interactableObjEvents.OnUngrab.AddListener(HandleReleaseAdd);
+
         }
 
-        private void HandleChange(object sender, Control3DEventArgs e)
+
+        //on CHANGE, display current value for this slider to help user gauge
+        private void HandleLivePreviewChange(object sender, Control3DEventArgs e)
         {
-            go.text = e.value.ToString() + "(" + e.normalizedValue.ToString() + "%)";
+            currentPreviewNumber = e.value;
+            dispalyTextMesh.text = currentPreviewNumber.ToString();
+            //dispalyTextMesh.text = currentPreviewNumber.ToString() + "(" + e.normalizedValue.ToString() + "%)";
+
+        }
+
+        //on RELEASE, Add to Adder
+        private void HandleReleaseAdd(object interactingObject, InteractableObjectEventArgs e) 
+            {
+            Adder.Add(currentPreviewNumber);
+            print("adder used");
         }
     }
 }
