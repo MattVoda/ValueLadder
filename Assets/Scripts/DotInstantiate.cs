35495d2f2f4df7a43b967af4a83ef25ac4f62fc3
MattVoda <mattvodakov@gmail.com>
2017-10-05 22:08:41 -0400

    WIP: added dots, endpoints, and Text Mesh Pro

---
+++ Assets/Scripts/DotInstantiate.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotInstantiate : MonoBehaviour {
+
+
+    public GameObject dot;
+    public GameObject minPoint;
+    public GameObject maxPoint;
+
+    public float delayInSec = 0.1f;
+
+    private int numPoints = 20;
+    private float distance;
+    private float segment;
+    
+
+    // Use this for initialization
+    void Start () {
+        distance = Vector3.Distance(maxPoint.transform.position, minPoint.transform.position);
+        segment = distance / numPoints;
+
+        StartCoroutine(DeployDots());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator DeployDots() {
+
+        //animate from center outwards
+
+        for (int z = 1; z <= numPoints / 2; z++) {
+            yield return new WaitForSeconds(delayInSec);
+
+            GameObject blockLeft = Instantiate(dot, Vector3.zero, dot.transform.rotation) as GameObject;
+            blockLeft.transform.parent = transform;
+            blockLeft.transform.localPosition = new Vector3(z * segment * -1, 0, 0);
+
+            GameObject blockRight = Instantiate(dot, Vector3.zero, dot.transform.rotation) as GameObject;
+            blockRight.transform.parent = transform;
+            blockRight.transform.localPosition = new Vector3(z * segment, 0, 0);
+        }
+
+        yield return null;
+    }
+
+}
